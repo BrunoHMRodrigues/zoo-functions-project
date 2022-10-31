f40b6c3ac74fc06c62b9b44cb70e69a0412e9a1c
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-31 16:28:00 -0300

    Realizado Requisito Obrigatório 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalData = species.find((element) => element.name === animal);
+  const { residents } = animalData;
+  const result = residents.reduce((allOlder, curr) => {
+    if (curr.age < age) {
+      return false;
+    }
+    return allOlder;
+  }, true);
+  return result;
 }
 
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
