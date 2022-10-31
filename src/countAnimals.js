3641e8379aaed7cdebfcb70c1845d6bbc4a2b63b
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-31 19:47:01 -0300

    Realizado Requisito Obrigatório 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
-const data = require('../data/zoo_data');
-
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = require('../data/zoo_data');
+// function countAnimals(animal) {
+//   const getAnimal = species.find((element) => (animal.specie === element.name));
+//   if (animal.sex) {
+//     return getAnimal.residents.reduce((total, curr) => total += (curr.sex === animal.sex) ? 1 : 0, 0);
+//   }
+//   return getAnimal.residents.length;
+// }
+function allAnimalsCountBySex(objAnimal, sex) {
+  return objAnimal.residents
+    .reduce((total, curr) => ((curr.sex === sex) ? total + 1 : total), 0);
 }
 
+function countAnimals(animal) {
+  if (animal) {
+    const getAnimal = species.find((element) => (animal.specie === element.name));
+    if (animal.sex) {
+      return allAnimalsCountBySex(getAnimal, animal.sex);
+    }
+    return getAnimal.residents.length;
+  }
+  const obj = species.reduce((acc, curr) => (
+    { ...acc, [curr.name]: curr.residents.length }
+  ), {});
+  return obj;
+}
+
+console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 module.exports = countAnimals;
