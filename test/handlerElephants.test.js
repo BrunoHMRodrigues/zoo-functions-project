7d384046ac831c9243a06dbeec593ea1e6b19bf9
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-31 20:39:02 -0300

    Realizado Requisito Obrigatório 06 e 07

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Confere se os parâmetros informados estão corretos', () => {
+    expect(handlerElephants('a')).toBe(null);
+    expect(handlerElephants()).toBe(undefined);
+  });
+  const alert = 'Parâmetro inválido, é necessário uma string';
+  it('Confere quando parâmetro informado não é uma string', () => {
+    expect(handlerElephants(5)).toBe(alert);
+    expect(handlerElephants([5, 4])).toBe(alert);
+    expect(handlerElephants({ produto: 5 })).toBe(alert);
+  });
+  it('Confere se os parâmetros informados estão corretos', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
