d27c3ae99f469c5dbc2d0949a630815545ab0797
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-31 17:13:40 -0300

    Realizado Requisito Obrigatório 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
+
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 
 function isManager(id) {
-  // seu código aqui
+  if (id === stephanieId || id === olaId || id === burlId) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    return employees.filter((employe) => employe.managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
