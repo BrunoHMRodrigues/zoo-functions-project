de9a763d9b178f2e6be209861a745d4c168093f3
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-31 15:50:45 -0300

    Realizado Requisito Obrigatório 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// Método 1
+// function arrayFilter(id) {
+//   return species.filter((animal) => animal.id === id);
+// }
+
+// function getSpeciesByIds(...ids) {
+//   return ids.map((idCode) => arrayFilter(idCode)[0]);
+// }
+
+// Método 2
+function getSpeciesByIds(...ids) {
+  return ids.map((idCode) => species.filter((animal) => animal.id === idCode)[0]);
 }
 
 module.exports = getSpeciesByIds;
