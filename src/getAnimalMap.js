5466ee179a94dca7325e95ac5b6ceaffbc1c37a2
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-01 17:06:50 -0300

    Limpando getAnimalMap para eliminar os erros de linter

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,4 +1,4 @@
-const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 const objResult = {
   NE: [],
@@ -8,17 +8,8 @@ const objResult = {
 };
 console.log(Object.keys(objResult));
 
-function createKeys() {
-  species.forEach((animal) => {
-    let AnimalLocation = animal.location;
-    Object.keys(objResult)
-  })
-}
-
 function getAnimalMap(options) {
-  if (options.includeNames) {
-    
-  }
+
 }
 
 module.exports = getAnimalMap;
