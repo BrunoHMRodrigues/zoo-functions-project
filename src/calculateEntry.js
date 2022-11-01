92e56028915b860dc5eef73a3630a532d36fcc1b
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-10-31 22:20:17 -0300

    Realizado Requisito Obrigatório 08

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function countEntrants(arrayEntrants) {
+  if (Array.isArray(arrayEntrants) && arrayEntrants !== [] && arrayEntrants !== [{}]) {
+    const countChilds = arrayEntrants.filter((child) => (child.age < 18)).length;
+    const countAdults = arrayEntrants.filter((adult) => (adult.age >= 18 && adult.age < 50)).length;
+    const countSeniors = arrayEntrants.filter((senior) => (senior.age >= 50)).length;
+    const objCountEntrants = { child: countChilds, adult: countAdults, senior: countSeniors };
+    return objCountEntrants;
+  }
+  return [];
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+function calculateEntry(objEntrants) {
+  if (!objEntrants) {
+    return 0;
+  }
+  const numberEntrants = countEntrants(objEntrants);
+  if (Object.keys(numberEntrants).length === 3) {
+    const totalChild = numberEntrants.child * 20.99;
+    const totalAdult = numberEntrants.adult * 49.99;
+    const totalSenior = numberEntrants.senior * 24.99;
 
+    return (totalChild + totalAdult + totalSenior);
+  }
+  return 0;
+}
+console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
